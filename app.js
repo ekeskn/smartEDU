fde4ab6c68439ff6016e6ac6beb0982e8b721cca
ekeskn <ege-keskintas@hotmail.com>
2022-04-16 23:50:06 +0300

    partials added

--- app.js
+++ app.js
@@ -2,8 +2,22 @@ const express = require('express');
 
 const app = express();
 
+//template engine
+app.set("view engine","ejs")
+
+//Middleware
+app.use(express.static("public"));
+
+//Routes
 app.get('/', (req, res) => {
-    res.send('Index');
+    res.status(200).render('index',{
+        page_name:"index"
+    });
+});
+app.get('/about', (req, res) => {
+    res.status(200).render('about',{
+        page_name: "about"
+    });
 });
 
 const port = 3000;
