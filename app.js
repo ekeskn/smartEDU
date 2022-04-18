f7579a957b64abdc825907ba3a80b7ff79f8d2d4
ekeskn <ege-keskintas@hotmail.com>
2022-04-19 01:45:26 +0300

    single pages added

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const pageRoute = require('./routes/pageRoutes');
+const courseRoute = require('./routes/courseRoute')
+
+
 
 const app = express();
+
+//Connect DB
+mongoose.connect('mongodb://localhost/smartedu-db')
+.then(()=>{
+    console.log('DB connected succesfully.')
+})
+
+
 
 //template engine
 app.set("view engine","ejs")
 
 //Middleware
 app.use(express.static("public"));
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 
 //Routes
-app.get('/', (req, res) => {
-    res.status(200).render('index',{
-        page_name:"index"
-    });
-});
-app.get('/about', (req, res) => {
-    res.status(200).render('about',{
-        page_name: "about"
-    });
-});
+app.use('/', pageRoute);
+app.use('/courses',courseRoute)
 
 const port = 3000;
 app.listen(port, () => {
