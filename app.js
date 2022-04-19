a62d6f75f537122994bebf2fb38cfa9a15c44884
ekeskn <ege-keskintas@hotmail.com>
2022-04-19 23:03:17 +0300

    categories and filtering added

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const pageRoute = require('./routes/pageRoutes');
-const courseRoute = require('./routes/courseRoute')
+const courseRoute = require('./routes/courseRoute');
+const categoryRoute = require('./routes/categoryRoute')
 
 
 
@@ -28,6 +29,7 @@ app.use(express.urlencoded({ extended: true })) // for parsing application/x-www
 //Routes
 app.use('/', pageRoute);
 app.use('/courses',courseRoute)
+app.use('/categories',categoryRoute)
 
 const port = 3000;
 app.listen(port, () => {
--- models/Course.js
+++ models/Course.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const slugify = require('slugify')
+const slugify = require('slugify');
 
 const CourseSchema = new Schema({
     name: {
@@ -16,21 +16,24 @@ const CourseSchema = new Schema({
     createdAt: {
         type: Date,
         default: Date.now,
-    
     },
     slug: {
         type: String,
         unique: true,
     },
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category',
+    },
 });
 
-CourseSchema.pre('validate',function(next){
-    this.slug = slugify(this.name,{
-        lower:true,
-        strict:true,
-    })
+CourseSchema.pre('validate', function (next) {
+    this.slug = slugify(this.name, {
+        lower: true,
+        strict: true,
+    });
     next();
-})
+});
 
 const Course = mongoose.model('Course', CourseSchema);
 
